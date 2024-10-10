85f4f6b4f9e11da72de17fdac4199720e332929e
Thinh Duc Le <thinh.duc.le@tul.cz>
2024-10-10 13:53:55 +0200

    #1 vstup hodnot a jejich vypis

--- IS-Projekty/001-prvni-program-vypis-rady/obj/Debug/net8.0/001-prvni-program-vypis-rady.AssemblyInfo.cs
+++ IS-Projekty/001-prvni-program-vypis-rady/obj/Debug/net8.0/001-prvni-program-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("001-prvni-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c975c632dd79196e8cf84ae183eb42568c08186d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb272f7efdeced733172e22a51d420db2b381ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("001-prvni-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("001-prvni-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
