8eb272f7efdeced733172e22a51d420db2b381ea
Thinh Duc Le <thinh.duc.le@tul.cz>
2024-10-10 13:31:54 +0200

    #1 zakladni program s moznosti opakovani

---
+++ IS-Projekty/001-prvni-program-vypis-rady/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program {
+
+    static void Main() {
+
+        //Po stisku klávesy "a" se program bude opakovat
+        string again = "a";
+        while(again == "a") {
+
+            Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("******* Výpis řady *******");
+            Console.WriteLine("*****************");
+            Console.WriteLine("****** Thinh Duc Le*******");
+            Console.WriteLine("**************************\n\n");
+            Console.WriteLine();
+
+            // Vstup od uživatele - TO DO
+
+            // Logika pro výpis řady - TO DO
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }
+}
