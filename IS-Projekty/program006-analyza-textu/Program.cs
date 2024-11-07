8b044f09a76429fe012fbe7fccb10709d9a5483b
Thinh Duc Le <thinh.duc.le@tul.cz>
2024-11-07 13:46:49 +0100

    #6 program analyza textu

---
+++ IS-Projekty/program006-analyza-textu/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Specialized;
+
+class Program {
+
+    static void Main() {
+
+        // chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("*****************************");
+            Console.WriteLine("******* Analýza textu *******");
+            Console.WriteLine("*****************************");
+            Console.WriteLine("******* Thinh Duc Le ********");
+            Console.WriteLine("*****************************\n\n");
+            Console.WriteLine();
+
+            // Vstup od uživatele 
+            Console.Write("\n Zadejte text pro analýzu: ");
+            string myText = Console.ReadLine();
+            string samohlasky = "aáeéiíoóuůúyý";
+            string souhlasky = "bcčdďfghjklmnňpqrřsštťvwxzž";
+            string cislice = "0123456789";
+
+            int pocetSam = 0;
+            int pocetSou = 0;
+            int pocetCis = 0;
+            int pocetOst = 0;
+
+            foreach (char znak in myText){
+                if (samohlasky.Contains(znak)) {
+                    pocetSam++;
+                } else if (souhlasky.Contains(znak)) {
+                    pocetSou++;
+                } else if (cislice.Contains(znak)){
+                    pocetCis++;
+                } else
+                    pocetOst++;
+            }
+
+            Console.WriteLine("\n\nPočet samohlásek: {0}", pocetSam);
+            Console.WriteLine("Počet souhlásek: {0}", pocetSou);
+            Console.WriteLine("Počet číslic: {0}", pocetCis);
+            Console.WriteLine("Počet ostatních: {0}", pocetOst);
+
+
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }   
+
+
+}
